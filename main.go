aa8c51a11fc951dfe4abcc4bcbfea550bf0dfe64
s1ntaxe770r <cyberdev01@protonmail.com>
2020-10-10 06:49:59 +0200

    fixed syncf but introduced bug

--- main.go
+++ main.go
@@ -15,20 +15,23 @@ type DotFile struct {
 // sync individual dotfile
 func syncf(d DotFile, repo string) error {
 	in, err := os.Open(d.Location + string(os.PathSeparator) + d.Name)
+	fmt.Println(d.Location + string(os.PathSeparator) + d.Name)
 	if err != nil {
-		return err
+		fmt.Printf("failed to open file %s", err)
+
 	}
 	defer in.Close()
 
 	out, err := os.Create(repo + string(os.PathSeparator) + d.Name)
 	if err != nil {
+		fmt.Printf("failed to create temp dir %s", err)
 		return err
 	}
 	defer out.Close()
 
 	_, err = io.Copy(out, in)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("failed to copy %s", err)
 	}
 
 	out.Close()
@@ -38,14 +41,16 @@ func syncf(d DotFile, repo string) error {
 
 func main() {
 	defaultdirs := map[string]string{
-		"nvimconf": "~/.config/nvim/",
+		"nvimconf": "~/.config/nvim",
 		"awesome":  "~/.config/awesome",
 		"zshrc":    "~/",
 	}
 
-	configrepo := "~/dots"
+	//	configrepo := "~/dots"
 	nvimconf := DotFile{"init.vim", defaultdirs["nvimconf"]}
-	awesomeconf := DotFile{"rc.lua", defaultdirs["awesome"]}
-	zshrc := DotFile{".zshrc", defaultdirs["zshrc"]}
+	//	awesomeconf := DotFile{"rc.lua", defaultdirs["awesome"]}
+	//	zshrc := DotFile{".zshrc", defaultdirs["zshrc"]}
+
+	syncf(nvimconf, "./")
 
 }
